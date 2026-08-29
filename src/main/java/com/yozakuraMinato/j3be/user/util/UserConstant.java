463584d35dd593fbea07f8b2e1b42c5c04095b9c
yozakura-minato <huynhhuulocht@gmail.com>
2026-08-29 17:23:20 +0700

    feat(user): mock user module

---
+++ src/main/java/com/yozakuraMinato/j3be/user/util/UserConstant.java
@@ -0,0 +1,11 @@
+package com.yozakuraMinato.j3be.user.util;
+
+import java.util.UUID;
+
+public class UserConstant {
+    public static class Temporary {
+        public static final UUID USER_ID = new UUID(0L, 0L);
+        public static final UUID HOST_ID = new UUID(0L, 0L);
+        public static final String HOST_PATH = "system";
+    }
+}
